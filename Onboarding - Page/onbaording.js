42a84fb7eb80e1b20512b63efbda225ca30b7388
ValeryMutetwa <valerymutetwa24@gmail.com>
2026-04-17 18:50:07 +0200

    hexagon background implementation

--- Onboarding - Page/onbaording.js
+++ Onboarding - Page/onbaording.js
@@ -1,18 +1,4 @@
 
-    function nextStep(step) {
-      document.querySelectorAll(".form-step").forEach(s => s.classList.remove("active"));
-      document.querySelector(`.form-step[data-step="${step}"]`).classList.add("active");
-
-      document.querySelectorAll(".dot").forEach(d => d.classList.remove("active"));
-      document.querySelector(`.dot[data-step="${step}"]`).classList.add("active");
-    }
-
-    function prevStep(step) {
-      nextStep(step);
-    }
-
-    document.getElementById("onboardingForm").addEventListener("submit", function(e) {
-      e.preventDefault();
    
    
    function nextStep(step) {
@@ -96,9 +82,4 @@
       window.addEventListener('resize', draw);
     })();
  
-  
-
-      // Handle form submission here
-      alert("Registration complete!");
-    });
   
